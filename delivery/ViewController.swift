6d8a52562a11b1143a18b61c58f052e0f2094740
nedostatok <xxxlvvtvvworkl@gmail.com>
2020-06-19 16:38:25 +0300

    initial com

---
+++ delivery/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  delivery
+//
+//  Created by User on 19.06.2020.
+//  Copyright © 2020 sad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var checkInOut: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        checkInOut.layer.cornerRadius = 15
+
+    }
+
+
+}
+
